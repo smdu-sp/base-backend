bc6c489f4fa964ba203f0bf13a21854141724a50
crscnc <dc3@mailbox.org>
2024-07-12 10:57:00 -0300

    Adição Prisma schemas

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,31 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 async function main() {
-    const root = await prisma.usuario.upsert({
-        where: { login: 'seu_login' },
-        create: {
-            login: 'seu_login',
-            nome: 'seu_nome',
-            email: 'seu_email',
-            status: 1,
-            permissao: 'DEV'
-        },
-        update: {
-            login: 'seu_login',
-            nome: 'seu_nome',
-            email: 'seu_email',
-            status: 1,
-            permissao: 'DEV'
-        }
-    });
-    console.log(root);
+  const root = await prisma.usuario.upsert({
+    where: { login: 'seu_login' },
+    create: {
+      login: 'seu_login',
+      nome: 'seu_nome',
+      email: 'seu_email',
+      status: 1,
+      permissao: 'DEV',
+    },
+    update: {
+      login: 'seu_login',
+      nome: 'seu_nome',
+      email: 'seu_email',
+      status: 1,
+      permissao: 'DEV',
+    },
+  });
+  console.log(root);
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
