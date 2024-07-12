07860190f46ab8ce5d6c7d8134ccc97676353e63
crscnc <dc3@mailbox.org>
2024-07-12 13:38:45 -0300

    Adição endpoint para verificac'ão de vitalidade

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,21 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { RoleGuard } from './auth/guards/role.guard';
 import { PrismaModule } from './prisma/prisma.module';
 import { Prisma2Module } from './prisma2/prisma2.module';
+import { VitalidadeModule } from './vitalidade/vitalidade.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [UsuariosModule, AuthModule, PrismaModule, Prisma2Module],
+  imports: [
+    UsuariosModule,
+    AuthModule,
+    PrismaModule,
+    Prisma2Module,
+    VitalidadeModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
